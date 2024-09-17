666710ea792f62f18df2b0960a3fd5ba37aa156f
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-17 13:43:31 +0530

    Updated

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,23 +1,9 @@
-import {useState, useEffect} from 'react';
+import { useLoaderData } from 'react-router-dom';
 import Post from './Post';
-import NewPost from './NewPost';
-import Modal from './Modal';
 import classes from './PostsList.module.css';
 
-function PostsList({isPosting, onStopPosting}) {
-const [posts, setPosts ] = useState([]);
-const [isFetching, setIsFetching]= useState(false); 
-
-useEffect(() => {
-    async function fetchPosts() {
-        setIsFetching(true);
-        const response = await fetch('http://localhost:8080/posts')
-        const resData = await response.json();
-        setPosts(resData.posts);
-        setIsFetching(false);
-    }
-    fetchPosts();   
-}, []);
+function PostsList() {
+const posts = useLoaderData();
 
 function addPostHandler(postData) {
     fetch('http://localhost:8080/posts', {
@@ -32,30 +18,19 @@ function addPostHandler(postData) {
 
     return ( 
         <>
-        {isPosting &&  (
-            <Modal onClose={onStopPosting}>
-                <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
-            </Modal>
-)}
-{!isFetching && posts.length > 0 && (
+{posts.length > 0 && (
     <ul className={classes.posts}>
         {posts.map((post) =>(
             <Post key={post.body} author={post.author} body={post.body}/>
         ))}
     </ul>
     )}
-    {!isFetching && posts.length === 0 && (
+    {posts.length === 0 && (
         <div style={{textAlign: 'center', color:'white'}}>
         <h2>There are no posts yet.</h2>
         <p>Get Started adding Some!</p>
         </div>
     )}
-    {!isFetching && (
-        <div style={{textAlign: 'center', color:'white'}}> 
-        <p>Loading posts...</p>
-        </div>
-        )
-    }
     </>   
     );
 }

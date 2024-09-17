bd6c346bbc0347ec649aee4f18243d1c2137d876
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-17 13:41:09 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
-import Posts from './routes/Posts'
+import Posts, {loader as postLoader} from './routes/Posts'
 import './index.css'
 import NewPost from './routes/NewPost';
 import RootLayout from './routes/RootLayout';
 
 const router = createBrowserRouter([
   {path: '/', element:<RootLayout/> , children: [
-    {path:'/', element:<Posts/>, 
+    {path:'/', element:<Posts/>, loader:postLoader,
     children: [{path:'/create-post', element:<NewPost/> }],
     },
   ],

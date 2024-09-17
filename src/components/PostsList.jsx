ca220e8dcb77fde19b7687074bca6c0150cd4a61
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-17 18:18:24 +0530

    Updated

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,23 +5,12 @@ import classes from './PostsList.module.css';
 function PostsList() {
 const posts = useLoaderData();
 
-function addPostHandler(postData) {
-    fetch('http://localhost:8080/posts', {
-        method: 'POST',
-        body: JSON.stringify(postData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-}
-
     return ( 
         <>
 {posts.length > 0 && (
     <ul className={classes.posts}>
         {posts.map((post) =>(
-            <Post key={post.body} author={post.author} body={post.body}/>
+            <Post key={post.id} id={post.id} author={post.author} body={post.body}/>
         ))}
     </ul>
     )}
